178b448929f3796d846cd911e5b9dd4b3c19bec7
ththfql <145094239+ththfql@users.noreply.github.com>
2026-06-17 15:38:08 +0900

    final

---
+++ Chp 13/Intl.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <algorithm>
+#include "Intl.h"
+#include "grade.h"
+
+using std::istream;
+
+Intl::Intl() : Core(), intl_exam(0) {}
+
+Intl::Intl(istream& is) { read(is); }
+
+istream& Intl::read(istream& is) {
+    Core::read_common(is); 
+    is >> intl_exam;       
+
+    if (is) {
+        homework.clear();
+        double x;
+        while (is >> x) {
+            homework.push_back(x);
+        }
+        is.clear();
+    }
+    return is;
+}
+
+double Intl::grade() const {
+   
+    return Core::grade();
+}
---
+++ Chp 13/Intl.h
@@ -0,0 +1,23 @@
+﻿#ifndef GUARD_Intl_h
+#define GUARD_Intl_h
+
+#include <iostream>
+#include "Core.h"
+#include "Vec.h" 
+
+class Intl : public Core {
+public:
+    Intl();
+    Intl(std::istream& is);
+
+    std::istream& read(std::istream& is) override;
+    double grade() const override;
+
+protected:
+    Intl* clone() const override { return new Intl(*this); }
+
+private:
+    double intl_exam; 
+};
+
+#endif
